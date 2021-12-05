162b61a51b0f757b76be4e1e2b3f5ecaa9bb93a2
devdeptraino1 <tao.gboy@gmail.com>
2021-12-05 16:01:42 +0700

    add db docker

--- config/settings/default.py
+++ config/settings/default.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fresherk1-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
